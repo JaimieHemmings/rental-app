f3e86ccf1b680f6ccc6d240d98be89d4934a6e93
Jaimie <jaimie.j.hemmings@gmail.com>
2024-05-11 09:33:16 +0100

    Added privacy policy

---
+++ app/privacyPolicy/page.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const privacyPolicy = () => {
+  return (
+    <>
+      <section className="bg-blue-700 py-20 mb-4">
+        <div
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+        >
+          <div className="text-center">
+            <h1
+              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+            >
+              Privacy Policy
+            </h1>
+            <p className="my-4 text-xl text-white">This privacy policy sets out how Dreckly Rentals uses and protects any information that you give Dreckly Rentals when you use this website. Dreckly Rentals is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement. Dreckly Rentals may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="container-xl lg:container m-auto">
+        <div className="text-center">
+          <p className="text-gray-800 mt-2 mb-4">What we Collect?</p>
+          <p className="text-gray-800 mt-2 mb-4">We may collect the following information - name and job title, contact information including email address, demographic information such as postcode, preferences and interests and other information relevant to customer surveys and/or offers.</p>
+          <p className="text-gray-800 mt-2 mb-4">What we do with the Information we Gather?</p>
+          <p className="text-gray-800 mt-2 mb-4">We require this information to understand your needs and provide you with a better service and in particular, for internal record keeping. We may also use the information to improve our products and services. Dreckly Rentals may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided. From time to time, we may also use your information to contact you for market research purposes. Dreckly Rentals may contact you by email, phone, fax or mail. We may use the information to customise the website according to your interests.</p>
+          <h2 className="text-gray-800 mt-2 mb-4 text-xl">Security</h2>
+          <p className="text-gray-800 mt-2 mb-4">We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure, Dreckly Rentals Ltd have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</p>
+          <p className="text-gray-800 mt-2 mb-4">Links to other Websites</p>
+          <p className="text-gray-800 mt-2 mb-4">Our website may contain links to other websites of interest. However, once you have used these links to leave our site, you should note that we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the website in question.</p>
+          <p className="text-gray-800 mt-2 mb-4">Controlling your Personal Information</p>
+          <p className="text-gray-800 mt-2 mb-4">You may choose to restrict the collection or use of your personal information for direct marketing purposes, by writing to us. Dreckly Rentals will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties, which we think you may find interesting if you tell us that you wish this to happen. You may request details of personal information which we hold about you under the Data Protection Act 1998. A small fee will be payable. If you would like a copy of the information held on you please contact us. If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the address on our contact page. We will promptly correct any information found to be incorrect.</p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default privacyPolicy
