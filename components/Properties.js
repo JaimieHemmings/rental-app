2f74252902c7403a02278b3aa471cc2e89b9c65c
Jaimie <jaimie.j.hemmings@gmail.com>
2024-05-12 21:13:40 +0100

    Corrected component Properties

--- components/Properties.js
+++ components/Properties.js
@@ -1,45 +1,22 @@
-import Link from 'next/link'
 import PropertyCard from '@/components/PropertyCard'
-import ConnectDB from '@/config/db'
-import Property from '@/models/Property'
+import Pagination from '@/components/Pagination'
 
-const HomeProperties = async () => {
-  await ConnectDB()
-
-  // Get the 3 latest properties
-  const recentProperties = await Property.find({})
-    .sort({ createdAt: -1 })
-    .limit(3)
-    .lean()
-
+const Properties = ({ properties, total, page, pageSize }) => {
   return (
-    <>
-      <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto'>
-          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
-            Recent Properties
-          </h2>
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {recentProperties.length === 0 ? (
-              <p>No Properties Found</p>
-            ) : (
-              recentProperties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))
-            )}
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
           </div>
-        </div>
-      </section>
-
-      <section className='m-auto max-w-lg my-10 px-6'>
-        <Link
-          href='/properties'
-          className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
-        >
-          View All Properties
-        </Link>
-      </section>
-    </>
+        )}
+        <Pagination page={page} pageSize={pageSize} totalItems={total} />
+      </div>
+    </section>
   )
 }
-export default HomeProperties
+export default Properties
